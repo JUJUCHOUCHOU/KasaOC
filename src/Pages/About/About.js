d611a698e3ab2cadfbcc921f2918900cecd13aa0
Julie <cohen.ju@laposte.net>
2023-08-16 11:40:51 +0200

    fix collapse

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,21 +4,20 @@ import Banner from '../../Components/Banner/Banner';
 import Navbar from '../../Components/Navbar/Navbar';
 import Collapse from '../../Components/Collapse/Collapse';
 import AboutDatas from '../../Data/AboutData.js';
-import Bannerabout from '../../Assets/bannerabout_image.png'
+import Bannerabout from '../../Assets/bannerabout_image.png';
 import './AboutStyle.scss';
 
 function About() {
   return (
     <div className="About">
       <Navbar />
-      <Banner image={Bannerabout}/>
+      <Banner image={Bannerabout} />
       <div className='AboutMain'>
-      {AboutDatas.map((item) => (
-       <Collapse
-       title={AboutDatas.title}>
-     <p>{AboutDatas.content}</p>
-     </Collapse>
-      ))}
+        {AboutDatas.map((item) => (
+          <Collapse title={item.title} key={item.id}>
+            <p>{item.content}</p>
+          </Collapse>
+        ))}
       </div>
       <Footer />
     </div>
